f27e772a385ae424d0ce0dcf3c8b71c138cbaeb9
Elena <femtochka@yahoo.com>
2022-10-24 15:24:24 +0300

    XO

--- X_O/controller.py
+++ X_O/controller.py
@@ -1,5 +1,6 @@
 import view
 import model
+import emoji
 def button_click():
     val = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     game_over = False
@@ -8,9 +9,9 @@ def button_click():
     while game_over == False:
         view.output_board(val)
         if player1 == True:
-            symbol, step = "X", int(input("игрок 1, ваш ход: "))
+            symbol, step = "X", int(input(emoji.emojize(":fire:игрок 1, ваш ход: ", language= 'en')))
         else:
-            symbol, step = "O", int(input("игрок 2, ваш ход: "))
+            symbol, step = "O", int(input(emoji.emojize("	:droplet: игрок 2, ваш ход: ", language= 'en')))
         val = model.take_input(step, symbol, val) 
         win = model.get_result(val) 
         if win == "":
@@ -20,4 +21,4 @@ def button_click():
         player1 = not(player1)   
 
     view.output_board(val)
-    print("Победил", win)
+    print(emoji.emojize(f":1st_place_medal: Победил {win}", language='en' ))
